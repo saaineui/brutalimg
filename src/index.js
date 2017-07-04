f0db9a25b1beedc2229bb3fb27f5f8ab81570de0
saaineui <saaineui@gmail.com>
2017-07-03 17:34:02 -0700

    search term via query string

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+var urlParams = new URLSearchParams(window.location.search);
+var searchTerm = urlParams.get("searchTerm") || "space cats";
+
 ReactDOM.render(
-  <App searchTerm="space cats" />,
+  <App searchTerm={searchTerm} />,
   document.getElementById('root')
 );
